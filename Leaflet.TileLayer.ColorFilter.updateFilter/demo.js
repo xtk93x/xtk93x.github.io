6019085b0cf124d5bfd5a486ec8ef2576d63a56c
Cláudio T. Kawakani <claudio.tk93@gmail.com>
2018-10-11 21:56:02 -0300

    New demo implemented.

--- Leaflet.TileLayer.ColorFilter.updateFilter/demo.js
+++ Leaflet.TileLayer.ColorFilter.updateFilter/demo.js
@@ -1,68 +1,56 @@
-let defaultMap = L.map('map-default').setView([51.505, -0.09], 14);
-let darkMap = L.map('map-dark').setView([51.505, -0.09], 14);
-let darkColorizedMap = L.map('map-dark-colorized').setView([51.505, -0.09], 14);
-let colorizedMap = L.map('map-colorized').setView([51.505, -0.09], 14);
-let colorized2Map = L.map('map-colorized-2').setView([51.505, -0.09], 14);
-let dark2Map = L.map('map-dark-2').setView([51.505, -0.09], 14);
+let darkMap = L.map('map').setView([51.505, -0.09], 14);
 
-let darkSettings = [
-    'grayscale:100%',
-    'invert:100%',
-]
-
-let darkColorizedSettings = [
-    'hue:185deg',
-    'invert:100%',
-]
-
-let colorizedSettings = [
-    'invert:30%',
-    'hue:130deg',
-    'saturate:700%',
-    'brightness:90%',
-    'contrast:200%',
-]
-
-let colorized2Settings = [
-    'brightness:110%',
-    'hue:90deg',
-    'saturate:120%',
-]
-
-
-let dark2Settings = [
-    'brightness:95%',
-    'contrast:130%',
-    'hue:290deg',
-    'invert:100%',
-    'saturate:300%',
-];
-
-L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+let darkTileLayer = L.tileLayer.colorFilter('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
     attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-}).addTo(defaultMap);
-
-L.tileLayer.colorFilter('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-    filter: darkSettings
+    filter: []
 }).addTo(darkMap);
 
-L.tileLayer.colorFilter('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-    filter: colorized2Settings
-}).addTo(colorized2Map);
 
-L.tileLayer.colorFilter('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-    filter: darkColorizedSettings
-}).addTo(darkColorizedMap);
+let invert = '100%';
+let bright = '100%';
+let gray = '0%';
+let hue = '0deg';
 
-L.tileLayer.colorFilter('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-    filter: colorizedSettings
-}).addTo(colorizedMap);
+let updateMyTiles = function () {
+    darkTileLayer.updateFilter([`grayscale:${gray}`, `invert:${invert}`, `bright:${bright}`, `hue:${hue}`]);
+}
 
-L.tileLayer.colorFilter('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-    filter: dark2Settings
-}).addTo(dark2Map);
+/* Invert slider */
+let invertSlider = document.getElementById('invert-slider');
+let invertLabel = document.getElementById('invert-value');
+invertSlider.addEventListener('input', function (e) {
+    let value = e.target.value;
+    invertLabel.textContent = value + '%';
+    invert = value + '%';
+    updateMyTiles();
+});
+
+/* Grayscale slider */
+let grayscaleSlider = document.getElementById('grayscale-slider');
+let grayscaleLabel = document.getElementById('grayscale-value');
+grayscaleSlider.addEventListener('input', function (e) {
+    let value = e.target.value;
+    grayscaleLabel.textContent = value + '%';
+    gray = value + '%';
+    updateMyTiles();
+});
+
+/* Brightness Slider */
+let brightnessSlider = document.getElementById('brightness-slider');
+let brightnessLabel = document.getElementById('brightness-value');
+brightnessSlider.addEventListener('input', function (e) {
+    let value = e.target.value;
+    brightnessLabel.textContent = value + '%';
+    bright = value + '%';
+    updateMyTiles();
+});
+
+/* Hue Slider */
+let hueSlider = document.getElementById('hue-slider');
+let hueLabel = document.getElementById('hue-value');
+hueSlider.addEventListener('input', function (e) {
+    let value = e.target.value;
+    hueLabel.textContent = value + 'deg';
+    hue = value + 'deg';
+    updateMyTiles();
+});
