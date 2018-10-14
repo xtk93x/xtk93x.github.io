a30c7ea476d730ee157cd4b83490a06512bc70ef
Cláudio T. Kawakani <claudio.tk93@gmail.com>
2018-10-14 11:18:48 -0300

    Tests

--- Leaflet.TileLayer.ColorFilter.liveTests/demo.js
+++ Leaflet.TileLayer.ColorFilter.liveTests/demo.js
@@ -2,7 +2,7 @@ let map = L.map('map').setView([51.505, -0.09], 14);
 
 let tileLayer = L.tileLayer.colorFilter('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
     attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-    filter: []
+    filter: ['sepia:100%']
 }).addTo(map);
 
 let blur = '0px';
